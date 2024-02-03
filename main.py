0211c2bf4f73e524255c556996809c6c55a8238f
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2024-02-03 15:38:27 +0300

    Exploratory Data Analysis almost done

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from datetime import date
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import LocalOutlierFactor
+import pandas as pd
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import (
     MinMaxScaler,
     LabelEncoder,
@@ -40,12 +43,13 @@ Features:
 
 
 Targets:
-    Underweight: Less than 18.5
-    Normal: 18.5 to 24.9
-    Overweight: 25.0 to 29.9
-    Obesity I: 30.0 to 34.9
-    Obesity II: 35.0 to 39.9
-    Obesity III: Higher than 40
+    Insufficient_Weight: label 0
+    Normal_Weight: label 1
+    Obesity_Type_I: label 2
+    Obesity_Type_II: label 3
+    Obesity_Type_III: label 4
+    Overweight_Level_I: label 5
+    Overweight_Level_II: label 6
 """
 
 # importin data from csv files
@@ -53,8 +57,15 @@ train = pd.read_csv("./data/train.csv")
 test = pd.read_csv("./data/test.csv")
 sample_submission = pd.read_csv("./data/sample_submission.csv")
 
+train["NObeyesdad"].value_counts()
+train["NObeyesdad_encoded"].value_counts()
 
-# checking data with check_train function
+#############################################
+# Exploratory Data Analysis
+#############################################
+
+
+# check_train function for checking the data
 def check_train(data_frame, head=5):
     print("##################### Shape #####################")
     print(data_frame.shape)
@@ -73,6 +84,7 @@ def check_train(data_frame, head=5):
 check_train(train)
 
 
+# grab column names for categorical, numerical, categorical but cardinal variables
 def grab_col_names(dataframe, cat_th=10, car_th=20):
     """we can use this function for getting categorical, numerical, categorical but cardinal variables names
 
@@ -149,7 +161,11 @@ def grab_col_names(dataframe, cat_th=10, car_th=20):
 
 cat_cols, num_cols, cat_but_car = grab_col_names(train)
 
+# drop the id column for outlier detection
+num_cols = [col for col in num_cols if "id" not in col]
 
+
+# Summary of categorical variabes
 def cat_summary(data_frame, colm_name, plot=False):
     print(
         pd.DataFrame(
@@ -175,6 +191,7 @@ for col in cat_cols:
         cat_summary(train, col, plot=True)
 
 
+# Summary of numerical variables
 def num_summary(dataframe, num_cols, plot=False):
     quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
     print(dataframe[num_cols].describe(quantiles).T)
@@ -186,6 +203,10 @@ def num_summary(dataframe, num_cols, plot=False):
         plt.show(block=True)
 
 
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_colwidth", None)  # Increase the width of the output
+
 for col in num_cols:
     # wrrite the variable name
     print(f"########################### {col} ###########################")
@@ -195,33 +216,178 @@ for col in num_cols:
 train.head()
 
 
-def target_summary_with_cat(dataframe, target, categorical_col):
-    # check the target variable data type and convert to int by using label encoder
-    if dataframe[target].dtypes == "object":
-        dataframe[target] = LabelEncoder().fit_transform(dataframe[target])
-
-    # calculate the percentage of each category
-    target_counts = (
-        dataframe.groupby(categorical_col)[target]
-        .value_counts(normalize=True)
-        .rename("percentage")
-        .mul(100)
-        .reset_index()
+# target variable analysis for categorical variables
+def target_summary_with_cat(dataframe, target, categorical_col, graph=False):
+    # label encoding for NObeyesdad column for visualization
+    label_encoder = LabelEncoder()
+    dataframe["NObeyesdad_encoded"] = label_encoder.fit_transform(
+        dataframe["NObeyesdad"]
     )
 
-    print(target_counts, end="\n\n")
+    # show percentage(?/100) of target variable for categorical variables
+    print(
+        dataframe.groupby(categorical_col)
+        .agg({target: ["count", "mean", "median", "std"]})
+        .sort_values(by=(target, "mean"), ascending=False)
+    )
+
+    # barplot for categorical variables
+    if graph:
+        sns.barplot(data=dataframe, x=categorical_col, y=target)
+        plt.show(block=True)
 
 
 for col in cat_cols:
-    target_summary_with_cat(train, "NObeyesdad", col)
+    print(f"##################### {col} #####################")
+    target_summary_with_cat(train, "NObeyesdad_encoded", col, graph=True)
 
 
+# target variable analysis for numerical variables
+def target_summary_with_num(dataframe, target, numerical_col, graph=False):
+    # show percentage(?/100) of target variable for numerical variables
+    print(
+        dataframe.groupby(target)
+        .agg({numerical_col: ["count", "mean", "median", "std"]})
+        .sort_values(by=(numerical_col, "mean"), ascending=False)
+    )
 
-def target_summary_with_num(dataframe, target, numerical_col):
-    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n")
+    # barplot for numerical variables
+    if graph:
+        sns.barplot(data=dataframe, x=target, y=numerical_col)
+        plt.show(block=True)
 
 
 for col in num_cols:
-    target_summary_with_num(train, "NObeyesdad", col)
+    print(f"##################### {col} #####################")
+    target_summary_with_num(train, "NObeyesdad_encoded", col, graph=True)
 
-# deneme 
+
+# Correlation matrix analysis for numerical variables
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    """
+    This function takes a pandas dataframe as input and returns a list of column names that have a correlation
+    coefficient greater than the specified threshold. If plot is set to True, it also displays a heatmap of the
+    correlation matrix using seaborn and matplotlib.
+
+    Parameters:
+    dataframe (pandas.DataFrame): The input dataframe.
+    plot (bool): Whether or not to display a heatmap of the correlation matrix. Default is False.
+    corr_th (float): The correlation threshold. Columns with a correlation coefficient greater than this value
+                     will be included in the returned list. Default is 0.90.
+
+    Returns:
+    list: A list of column names that have a correlation coefficient greater than the specified threshold.
+    """
+    # Calculate the correlation matrix
+    corr_matrix = dataframe.corr().abs()
+
+    # Create a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
+
+    # Create a series with the correlation coefficients
+    high_corr = corr_matrix.mask(mask).stack().sort_values(ascending=False)
+
+    # Filter the series to include only the correlations above the threshold
+    high_corr = high_corr[high_corr > corr_th]
+
+    # Convert the series index to a list of tuples
+    high_corr.index = high_corr.index.to_list()
+
+    # Create a list of the column names
+    high_corr_list = [x[0] for x in high_corr.index]
+
+    # Display the heatmap
+    if plot:
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(corr_matrix, mask=mask, annot=True, cmap="coolwarm", fmt=".2f")
+        plt.title("Correlation Heatmap")
+        plt.show()
+
+    return high_corr_list
+
+
+train.shape
+
+high_corr_list = high_correlated_cols(train, plot=True, corr_th=0.90)
+
+# drop highly correlated columns
+train = train.drop(high_corr_list, axis=1)
+
+train.shape
+
+#############################################
+# Outliers
+#############################################
+
+
+# outlier_thresholds function for finding the limits of outliers
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+# check_outlier function for checking the outliers
+def check_outlier(dataframe, col_name, q1=0.25, q3=0.75):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name, q1, q3)
+    if dataframe[
+        (dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)
+    ].any(axis=None):
+        return True
+    else:
+        return False
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(train)
+
+# drop id from num cols
+num_cols = [col for col in num_cols if "id" not in col]
+
+
+train.head()
+
+
+# grab_outliers function for grabbing the outliers
+def grab_outliers(dataframe, col_name, index=False):
+    low, up = outlier_thresholds(dataframe, col_name)
+
+    if (
+        dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0]
+        > 10
+    ):
+        print(
+            dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head()
+        )
+    else:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+
+    if index:
+        outlier_index = dataframe[
+            ((dataframe[col_name] < low) | (dataframe[col_name] > up))
+        ].index
+        return outlier_index
+
+
+# remove_outlier function for removing the outliers
+def remove_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[
+        ~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))
+    ]
+    return df_without_outliers
+
+
+# replace_with_thresholds function for replacing the outliers with the limits
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+# outlier analysis for numerical variables
+for col in num_cols:
+    print(f"##################### {col} #####################")
+    print(col, check_outlier(train, col, q1=0.05, q3=0.95))
